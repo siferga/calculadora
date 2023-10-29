5b194b89c964d52d1c9f549b5e18d86af1c1850c
Siferga <sifergadeveloper@gmail.com>
2023-10-29 16:39:28 +0100

    onClick button

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,11 +4,13 @@ import '../styleSheets/Button.css';
 function Button(props) {
 
   const isOperator = valor => {
-    return isNaN(valor) && (valor != '.') && (valor != '=');
+    return isNaN(valor) && (valor !== '.') && (valor !== '=');
   };
+
   return (
     <div
-      className={`button-container ${isOperator(props.children) ? 'operator' : null}`}>
+    className={`button-container ${isOperator(props.children) ? 'operator' : ''}`.trimEnd()}
+    onClick={() => props.controlClicK(props.children)}>
       {props.children}
     </div>
   );
