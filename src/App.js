6494b90396e0143af27329a45957f10297eec4e0
Siferga <sifergadeveloper@gmail.com>
2023-10-29 14:18:46 +0100

    buttons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
 import './App.css';
-import freeCodeCampLogo from '.images/freecodecamp-logo.png'
+import freeCodeCampLogo from './images/freecodecamp-logo.png'
+import Button from './components/Button';
 
 function App() {
   return (
     <div className='App'>
-       <div className='freecodecamp-logo-contenedor'>
+      <div className='freecodecamp-logo-container'>
         <img 
           src={freeCodeCampLogo}
           className='freecodecamp-logo'
           alt='Logo freeCodeCamp' />
       </div>
+      <div className='calculator-container'>
+        <div className='line'>
+          <Button>1</Button>
+          <Button>2</Button>
+          <Button>3</Button>
+          <Button>+</Button>
+        </div>
+        <div className='line'>
+          <Button>4</Button>
+          <Button>5</Button>
+          <Button>6</Button>
+          <Button>-</Button>
+        </div>
+        <div className='line'>
+          <Button>7</Button>
+          <Button>8</Button>
+          <Button>9</Button>
+          <Button>*</Button>
+        </div>
+        <div className='line'>
+          <Button>=</Button>
+          <Button>0</Button>
+          <Button>.</Button>
+          <Button>/</Button>
+        </div>
+        <div className='line'></div>
+      </div>
+
     </div>
   );
 }
